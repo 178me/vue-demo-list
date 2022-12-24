4dbf4bdebd6c8303e11b8efcdde89c27097bbd86
yzl178me <1403341393@qq.com>
2022-12-24 22:30:44 +0800

    fix: update

--- src/pages/A.vue
+++ src/pages/A.vue
@@ -1,64 +1,43 @@
 <template>
   <div flex="~ col" w-screen h="screen">
     <span text="center 25px">测试拖拽</span>
-    <div bg-red w-full h-100px>拖动到这儿</div>
-    <div class="overflow-hidden" grid="~ cols-3 gap-10px">
-      <div
-        @pointerdown="hdlDown"
-        @pointermove="hdlMove"
-        @pointerup="hdlUp"
-        style="touch-action: none"
-        v-for="i in 30"
-        flex="~ col"
-        items-center>
-        <div h-100px w-100px bg-blue-500 />
-        <span only="text-15px">{{ i }}</span>
+    <div class="overflow-auto" grid="~ cols-3 gap-10px">
+      <div v-for="i in 30" flex="~ col" items-center>
+        <Draggable
+          v-slot="{ isDragging }"
+          @start="showDropZone = true"
+          @end="showDropZone = false"
+          @dropover="hdlEnter"
+          :drop-el="dropEl">
+          <div h-50px w-50px bg-blue-500 />
+          <span v-if="!isDragging" only="text-15px">{{ i }}</span>
+        </Draggable>
       </div>
     </div>
+    <transition
+      enter-active-class="animate-fade-in-up animate-duration-350"
+      leave-active-class="animate-fade-out-down animate-duration-350">
+      <div
+        v-if="showDropZone"
+        @touchmove="hdlEnter"
+        ref="dropEl"
+        bg-red
+        w-full
+        h-200px>
+        拖动到这儿
+      </div>
+    </transition>
   </div>
 </template>
 <script setup lang="ts">
 import { ref, onMounted } from "vue"
-import { useRouter } from "vue-router"
-import { useSharedData } from "./useData"
-import { useDraggable } from "@vueuse/core"
+import Draggable from "@/components/draggable/index.vue"
+const dropEl = ref<null | HTMLElement>(null)
+const showDropZone = ref(false)
 
-const isDragging = ref(false)
-const dragEl = ref<null | HTMLElement>(null)
-const startStyle = ref({ left: 0, top: 0, touchAction: "", position: "" })
-// const { x, y, position } = useDraggable(dragEl)
-
-function hdlDown(e: PointerEvent) {
-  console.info("178me-debug:",e)
-  isDragging.value = true
-  dragEl.value = e.target
-  startStyle.value.left = e.offsetX
-  startStyle.value.top = e.offsetY
-  startStyle.value.position = dragEl.value?.style.position || ""
-  dragEl.value.style.position = "fixed"
-  dragEl.value.style.touchAction = "none"
-  dragEl.value.style.left = `${e.clientX - 50}px`
-  dragEl.value.style.top = `${e.clientY - 50}px`
-  console.info("178me-debug:down", e.offsetX, e.offsetY)
-}
-function hdlUp(e: PointerEvent) {
-  isDragging.value = false
-  dragEl.value.style.top = startStyle.value.top
-  dragEl.value.style.left = startStyle.value.left
-  dragEl.value.style.position = startStyle.value.position
-  dragEl.value = null
-  console.info("178me-debug:up", e.offsetX, e.offsetY)
+function hdlEnter() {
+  alert("拖放成功")
 }
 
-function hdlMove(e: PointerEvent) {
-  if (!isDragging.value) return
-  if (!dragEl.value) return
-  dragEl.value.style.left = `${e.clientX - 50}px`
-  dragEl.value.style.top = `${e.clientY - 50}px`
-  console.info("178me-debug:", e.clientX, e.clientY)
-}
-
-// window.addEventListener("pointerdown", hdlDown)
-// window.addEventListener("pointermove", hdlMove)
-// window.addEventListener("pointerup", hdlUp)
+onMounted(() => {})
 </script>
