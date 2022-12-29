4d68692c727c7f9b8677e1b4c60ddcf405bc05a0
yzl178me <1403341393@qq.com>
2022-12-29 23:48:37 +0800

    feat: page layout

---
+++ src/layout/page/index.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+export default {
+  inheritAttrs: false,
+}
+</script>
+<script setup lang="ts">
+defineProps<{
+  title?: string
+}>()
+</script>
+
+<template>
+  <div flex="~ col" w="screen" h="screen">
+    <span text="center 25px">{{ title }}</span>
+    <div v-bind="$attrs" grow><slot></slot></div>
+  </div>
+</template>
