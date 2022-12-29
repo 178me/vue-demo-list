5f049827203a67e7aec79d4f49ed55ab82986452
yzl178me <1403341393@qq.com>
2022-12-29 23:49:05 +0800

    feat: scrollBar demo

---
+++ src/pages/demo/Scroll.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+import ScrollBar from "@/components/scroll-bar/index.vue"
+</script>
+
+<template>
+  <div class="w-screen h-screen overflow-hidden flex flex-col">
+    <div h-100px></div>
+    <ScrollBar>
+      <div h="500px" w="full" bg="gray" overflow="auto" my-0px>
+        <div v-for="it in 300" text-center pr-13px>
+          <div h="50px">
+            {{ it }}
+          </div>
+        </div>
+      </div>
+    </ScrollBar>
+  </div>
+</template>
